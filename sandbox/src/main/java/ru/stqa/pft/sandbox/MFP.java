ae77abd476e8ec23050e3dc0a118dfc2268dee56
Kamilya <65188242+kamseyfulina@users.noreply.github.com>
2020-05-16 14:19:40 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MFP.java
@@ -1,10 +1,9 @@
 package ru.stqa.pft.sandbox;
 
-public class MFP
-{
- public static void main(String[] args)
- {
-    System.out.println("Hello, world!");
- }
+public class MFP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
 }
