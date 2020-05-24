3ac3a56dc4253010bd8ce8e70434a0f3ef0d59af
Kamilya <65188242+kamseyfulina@users.noreply.github.com>
2020-05-24 15:57:46 +0300

    Доп.коммит к заданию 4: afterclass и beforeclass изменены на aftermethod и beforemethod

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 public class ContactCreationTests {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -84,7 +84,7 @@ public class ContactCreationTests {
     wd.findElement(By.linkText("add new")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     logout();
     wd.quit();
