781487d2ecccbb81232c65983aa04a025f32ded8
Kamilya <65188242+kamseyfulina@users.noreply.github.com>
2020-06-28 23:31:13 +0300

    Добавлено логироание

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
@@ -14,9 +22,19 @@ public class TestBase {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite (alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object [] p) {
+    logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod (alwaysRun = true)
+  public void logTestStop(Method m, Object [] p) {
+    logger.info("Stop test " + m.getName() + " with parameters " + Arrays.asList(p));
   }
 
 }
